3bb193da2ba724d0d31f77e66f71d3ffc39a0c95
Dibya <dibyachakravorty@gmail.com>
2017-09-09 10:42:21 +0200

    script for polling spotify

---
+++ spotify/poll.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import re
+
+from spotipy import Spotify
+import spotipy.util as util
+import requests
+
+class MySpotify(Spotify):
+
+    def currently_playing_track(self):
+        return self._get("me/player/currently-playing")
+
+scope = 'user-read-currently-playing'
+username = os.environ['SPOTIFY_USER_ID']
+
+token = util.prompt_for_user_token(username, scope)
+
+if token:
+    sp = MySpotify(auth=token)
+    result = sp.currently_playing_track()
+    artist_href = result["item"]["artists"][0]["href"]
+    pattern = re.compile("artists/(.+)")
+    match = re.search(pattern, artist_href)
+    artist_code = match.group(1)
+    artist_result = sp.artist(artist_code)
+    image_url = artist_result["images"][0]
+else:
+    print "Can't get token for", username
