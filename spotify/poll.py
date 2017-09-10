9fdfa749289dc604a96e3b85f75d935a3da711de
Dibya <dibyachakravorty@gmail.com>
2017-09-10 09:27:40 +0200

    lots of changes

--- spotify/poll.py
+++ spotify/poll.py
@@ -9,6 +9,10 @@ from spotipy import Spotify
 import spotipy.util as util
 import requests
 import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+from unipath import Path
+from colorthief import ColorThief
 
 class MySpotify(Spotify):
 
@@ -26,6 +30,7 @@ first_image = True
 plt.ion()
 plt.axis("off")
 plt.show()
+image_file_path = None
 
 if token:
     while True:
@@ -37,24 +42,55 @@ if token:
             continue
         track_id = result["item"]["id"]
         if track_id != last_id:
+            if image_file_path:
+                os.remove(image_file_path)
             artist_href = result["item"]["artists"][0]["href"]
             pattern = re.compile("artists/(.+)")
             match = re.search(pattern, artist_href)
             artist_code = match.group(1)
             artist_result = sp.artist(artist_code)
-            image_url = artist_result["images"][0]["url"]
-            print(image_url)
+            #image_url = artist_result["images"][0]["url"]
+            image_url = result["item"]["album"]["images"][0]["url"]
+            #print(image_url)
             last_id = track_id
 
-            # display image in matplotlib
+            # download image
             response = requests.get(image_url)
-            f = BytesIO(response.content)
-            im = plt.imread(f, format = "jpeg")
+            bytestream = BytesIO(response.content)
+
+            # resize image using pillow
+            pil_image = Image.open(bytestream)
+            pil_image.thumbnail([480, 480])
+            cropped_pil_image = pil_image.crop((0, 80, 480, 400))
+            cropped_pil_image.load()
+
+            # convert to a numpy array
+
+            pil_image_numpy_array = np.array(cropped_pil_image)
+
+            # save to temporary file
+            image_file_path = Path(__file__).ancestor(1).child(
+                "{0}.jpeg".format(artist_code)
+                )
+            cropped_pil_image.save(image_file_path)
+
+            # get dominant colors
+            color_thief = ColorThief(image_file_path)
+            dominant_color = color_thief.get_color(quality=1)
+            #print(dominant_color)
+
+            # display image in matplotlib
+            #im = plt.imread(pil_image_numpy_array)
             if first_image:
-                imshow_object = plt.imshow(im)
+                imshow_object = plt.imshow(pil_image_numpy_array)
             else:
-                imshow_object.set_data(im)
+                imshow_object.set_data(pil_image_numpy_array)
             plt.pause(0.001)
             plt.draw()
+
+            # fullscreen image
+            command = ["sudo", "fbi", "-T", "2", "-d", "/dev/fbi", "noverbose",
+                "-a", image_file_path
+                ]
 else:
     print "Can't get token for", username
